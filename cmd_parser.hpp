2a0c5908ce2532c6428367ff08ae18a3b87b26f9
Dicridon <noahxiong@outlook.com>
2021-09-29 13:33:29 +0800

    get_as now is const

--- cmd_parser.hpp
+++ cmd_parser.hpp
@@ -140,7 +140,7 @@ namespace CmdParser {
         }
 
         template<typename Target>
-        auto parse_value(const std::string &in) -> std::optional<Target> {
+        auto parse_value(const std::string &in) const -> std::optional<Target> {
             Target holder;
             std::stringstream buf(in);
             buf >> holder;
@@ -160,7 +160,7 @@ namespace CmdParser {
 
         template<typename T,
                  typename = typename std::enable_if_t<std::is_same_v<T, bool>>>
-        auto get_as(const std::string &opt) -> std::optional<bool> {
+        auto get_as(const std::string &opt) const -> std::optional<bool> {
             auto maybe_parsed = parsed_map.find(opt);
 
             // find a parsed value
@@ -192,7 +192,7 @@ namespace CmdParser {
 
         template<typename Target,
                  typename = typename std::enable_if_t<!std::is_same_v<Target, bool>>>
-        auto get_as(const std::string &opt) -> std::optional<Target> {
+        auto get_as(const std::string &opt) const -> std::optional<Target> {
             auto maybe_parsed = parsed_map.find(opt);
 
             // find a parsed value
@@ -220,7 +220,7 @@ namespace CmdParser {
 
         template<typename Target>
         auto get_as(const std::string &opt,
-                    std::function<std::optional<Target>(const std::string &s)> convertor) -> std::optional<Target> {
+                    std::function<std::optional<Target>(const std::string &s)> convertor) const -> std::optional<Target> {
 
             auto plain = plain_map.find(opt);
             if (plain == plain_map.end()) {
@@ -251,7 +251,7 @@ namespace CmdParser {
         static const std::string regex_short_suffix;
         OptionMap plain_map;
         RegexMap regex_map;
-        ParsedOptionMap parsed_map;
+        mutable ParsedOptionMap parsed_map;
     };
 
 }
